a6818ce6c98757d059834b8c3107ca8dc2922e79
aryan <paryan@digitalocean.com>
2024-07-14 21:36:51 +0200

    Update config

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,7 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
